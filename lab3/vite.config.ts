00de855972080a0df680095412ea293c0265b257
vinhhotff <thanvinh1602@gmail.com>
2025-02-24 22:57:03 +0700

    Update vite.config.ts

--- lab3/vite.config.ts
+++ lab3/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path"; // Import path để cấu hình alias
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // Thêm alias cho "@"
+    },
+  },
   base: "/",
   build: {
     outDir: "dist"
