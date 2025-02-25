2f3b2378f95c2c95c412463da56290120e35e99c
vinhhotff <thanvinh1602@gmail.com>
2025-02-25 08:05:00 +0700

    Update Login.tsx

--- lab3/src/components/Pages/Login.tsx
+++ lab3/src/components/Pages/Login.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Link, Links } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
   const form = useForm({
